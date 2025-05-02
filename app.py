b8e2ec0e8954e65f683cb0fea07c7c4d65802407
Teloshav <zpapq3zvvv9y3li@tutanota.com>
2025-05-02 16:59:35 +0800

    Set up initial flask structure. Renamed some folders.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import yaml
+
+with open('config.yml', 'r') as file:
+    yaml_options = yaml.safe_load(file)
+
+app = Flask(__name__)
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
+
+if __name__ == "__main__":
+    app.run(debug= True, port= yaml_options["server_port"])
